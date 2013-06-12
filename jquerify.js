4c518833bb92fbbd93154b61dc496af6f60b4949
bgrins <briangrinstead@gmail.com>
2013-06-12 14:44:30 -0500

    use features from Issue #1

--- jquerify.js
+++ jquerify.js
@@ -1,15 +1,10 @@
 // https://github.com/bgrins/devtools-snippets
 // Add jQuery to any page
-	
+
 (function () {
     if ( !window.jQuery ) {
-        var jQ = document.createElement('script');
-        var prop = typeof jQ.src;
-        if ( prop != 'undefined' && prop.toLowerCase() == 'string' )
-            jQ.src = '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js';
-        else
-            jQ.setAttribute( 'src', '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js' );
-        
-        document.body.appendChild( jQ );
+        var s=document.createElement('script');
+        s.setAttribute('src','//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js');
+        document.body.appendChild(s);
     }
 })();
