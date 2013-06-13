071e23d69fff215f0e78871ba332108d140acd1a
bgrins <briangrinstead@gmail.com>
2013-06-12 20:29:57 -0500

    add log snippet

---
+++ snippets/log.js
@@ -0,0 +1,10 @@
+// log.js
+// https://github.com/bgrins/devtools-snippets
+// Adds a `log` function to window object
+// http://www.briangrinstead.com/blog/console-log-helper-function
+
+(function() {
+
+  window.log = Function.prototype.bind.call(console.log, console);
+
+})();
