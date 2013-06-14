112a53e4dc7ff01ce604f0c2517fc6ab97fc85aa
fmovlex <fmovlex@gmail.com>
2013-06-14 17:23:36 +0300

    Additional dive into nodes, grouped by color
    Helpful for hunting down the nodes using whatever color you're interested in, especially in complex pages.
    
    Here's what it looks like: http://s21.postimg.org/6hqpuy4kn/Screenshot_from_2013_06_14_17_12_47.png

--- snippets/allcolors.js
+++ snippets/allcolors.js
@@ -2,32 +2,51 @@
 // https://github.com/bgrins/devtools-snippets
 // Print out all colors used in elements on the page
 
-(function() {
-
-  var allColors = { };
+(function () {
+  var allColors = {};
   var props = ["backgroud-color", "color"];
 
-  [].forEach.call(document.querySelectorAll("*"), function(node) {
-
-    props.forEach(function(prop) {
+  [].forEach.call(document.querySelectorAll("*"), function (node) {
+    props.forEach(function (prop) {
       var color = window.getComputedStyle(node, null).getPropertyValue(prop);
       if (color && color != "rgb(0, 0, 0)" && color != "rgb(255, 255, 255)") {
-        allColors[color] = allColors[color] ? allColors[color] + 1 : 1;
+        if (!allColors[color])
+          allColors[color] = {
+            count: 0,
+            nodes: []
+          };
+
+        allColors[color].count++;
+        allColors[color].nodes.push(node);
       }
     });
-
   });
 
   var allColorsSorted = [];
   for (var i in allColors) {
-    allColorsSorted.push({ key: i, value: allColors[i] });
+    allColorsSorted.push({
+      key: i,
+      value: allColors[i]
+    });
   }
-  allColorsSorted = allColorsSorted.sort(function(a, b) { return b.value-a.value; });
+  allColorsSorted = allColorsSorted.sort(function (a, b) {
+    return b.value.count - a.value.count;
+  });
+
+  var nameStyle = "font-weight:normal;";
+  var countStyle = "font-weight:bold;";
+  var colorStyle = function (color) {
+    return "background:" + color + ";color:" + color + ";border:1px solid #333;";
+  };
 
   console.group("All colors used in elements on the page");
-  allColorsSorted.forEach(function(c) {
-    console.log("%c____%c " + c.key + " %c(" + c.value + " times)", "background:" + c.key + ";color:" + c.key + ";border:1px solid #333;", "", "font-weight:bold;");
+  allColorsSorted.forEach(function (c) {
+    console.groupCollapsed("%c____%c " + c.key + " %c(" + c.value.count + " times)",
+      colorStyle(c.key), nameStyle, countStyle);
+    c.value.nodes.forEach(function (node) {
+      console.log(node);
+    });
+    console.groupEnd();
   });
   console.groupEnd("All colors used in elements on the page");
-
 })();
