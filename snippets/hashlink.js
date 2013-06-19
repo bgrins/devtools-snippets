d8007c6cb2e7bb33d8fa5f3235e12e20e032365b
bgrins <briangrinstead@gmail.com>
2013-06-19 10:57:54 -0500

    add hashlink snippet

---
+++ snippets/hashlink.js
@@ -0,0 +1,49 @@
+// hashlink.js
+// https://github.com/bgrins/devtools-snippets
+// Click on an element to print out the closest hash link
+
+(function() {
+
+    function logHashlink(e) {
+
+        document.removeEventListener('mousedown', logHashlink, true);
+
+        var node = e.target;
+        var id = null;
+        while (node != null) {
+
+            if (node.tagName === "A" && node.name) {
+                id = node.name;
+                break;
+            }
+
+            if (node.id) {
+                id = node.id;
+                break;
+            }
+
+            node = node.parentNode;
+        }
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        var URL = window.location.origin + window.location.pathname + window.location.search;
+
+        console.group("Hashlink");
+        console.log("Clicked on ", e.target);
+        if (id === null) {
+            console.log("No ID Found - closest anchor: " +  URL);
+        }
+        else {
+            console.log("Closest linkable element: ", node);
+            console.log(URL + "#" + id);
+        }
+        console.groupEnd("Hashlink");
+    }
+
+    document.addEventListener('mousedown', logHashlink, true);
+
+    return "haslink: Click on an element to log it's closest hash link";
+
+})();
