af59836b21266b5deeda8a1c4f4a4ecf24be5383
Declan Cook <DeclanCook44@gmail.com>
2013-10-21 23:09:48 +0100

    Console.save snippet
    Extend the console object to add a save function to save javascript
    objects to json files.

---
+++ snippets/console-save/README.md
@@ -0,0 +1,13 @@
+# Console.save
+
+A simple way to save objects as .json fle from the console, includes a chrome extension along with a plain script.
+
+## Usage
+
+`console.save(filename, data)`
+
+Data can be a string or just an object, objects are passed through `json.stringify()` before writing to file.
+
+## Licence
+
+MIT
---
+++ snippets/console-save/console-save.js
@@ -0,0 +1,24 @@
+(function(console){
+
+    console.save = function(filename, data){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data)
+        }
+
+        var blob = new Blob([data], {type: 'text/json'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+    }
+})(console)
