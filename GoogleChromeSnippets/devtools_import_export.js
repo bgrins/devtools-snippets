d46ec1a448ef58726cd6ff6c865f0c0581317ba6
Adrian Aichner <adrian.aichner@gmail.com>
2013-10-30 23:56:22 +0100

    Integrate latest functionality of collect_command_history.js
    Drop window.open strWindowFeatures argument to open in tabs instead of popups, still via anchor click listeners.

--- GoogleChromeSnippets/devtools_import_export.js
+++ GoogleChromeSnippets/devtools_import_export.js
@@ -9,6 +9,114 @@
 // at 2013-10-29T00:31:54.370Z
 (function() {
     try {
+        function downloadConsoleHistory() {
+    try {
+        function alertAndWarn(message) {
+            window.alert(message + "\nSee JavaScript console the complete log of warnings");
+            console.warn(message);
+        }
+        function selectCommand(event, prevent, stop) {
+            if (prevent) {
+                event.preventDefault();
+            }
+            if (stop) {
+                event.stopPropagation();
+            }
+            var selection = window.getSelection();
+            selection.removeAllRanges();
+            var range = document.createRange();
+            range.selectNodeContents(event.target);
+            // Collapse range to end, i.e. toStart argument is false.
+            // range.collapse(false);
+            // Always add the range to restore focus.
+            selection.addRange(range);
+        }
+        function setupEventListeners(element) {
+            element.addEventListener('mouseenter', selectCommand, true);
+            element.addEventListener('dragstart', function(event) {
+                event.dataTransfer.effectAllowed = "copy";
+            }, false);
+            element.addEventListener('drop', function(event) {
+                event.dataTransfer.effectAllowed = "none";
+            }, false);
+            element.addEventListener('keydown', function(event) {
+                selectCommand(event, false, true)
+            }, true);
+            element.addEventListener('keypress', function(event) {
+                selectCommand(event, true, true)
+            }, true);
+            element.addEventListener('keyup', function(event) {
+                selectCommand(event, false, true)
+            }, true);
+        }
+        function installGeneratedHtmlEventListeners() {
+            document.addEventListener('readystatechange', function(event) {
+                if (event.target.readyState !== 'complete') {
+                    return;
+                }
+                (function() {
+                    var nds = document.querySelectorAll('div>pre');
+                    for (var i = 0, len = nds.length; i < len; i++) {
+                        setupEventListeners(nds[i]);
+                    }
+                    // TODO Please note the Download button would be confusing in the generated document.
+                    document.body.removeChild(document.querySelector('.download').parentElement);
+                })();
+            }, false);
+        }
+            var w = window.open("", "");
+//         var w = window.open("", "dtie_ch", "width=" + window.screen.availWidth / 2 + ",height=" + window.screen.availHeight / 2);
+//         var w = window;
+        var meta = document.createElement('meta');
+        meta.content = "text/html; charset=UTF-8";
+        meta.httpEquiv = "content-type";
+        w.document.head.appendChild(meta);
+        var script = document.createElement('script');
+        // TODO Why the \/ (see https://developer.mozilla.org/en-US/docs/Web/API/HTMLScriptElement)?
+        script.type = "text\/javascript";
+        script.textContent = '"use strict"' + ';\n' + selectCommand + ';\n' + setupEventListeners + ';\n' + installGeneratedHtmlEventListeners + ';\n' + 'installGeneratedHtmlEventListeners();\n';
+        w.document.head.appendChild(script);
+        var style = document.createElement('style');
+        style.type="text/css";
+        style.textContent = 'pre:before { content: attr(line-number); float: left; text-align: right; width: 2em; padding-right: 1em; }\n';
+        w.document.head.appendChild(style);
+        var now = new Date();
+        var ch = JSON.parse(localStorage.consoleHistory);
+        w.document.title = "consoleHistory (" + ch.length + ' commands)';
+        var consoleHistoryDownloadButton = document.createElement('input');
+        consoleHistoryDownloadButton.type = 'button';
+        consoleHistoryDownloadButton.className = 'download';
+        consoleHistoryDownloadButton.value = 'Download consoleHistory';
+        w.document.body.appendChild(document.createElement('div').appendChild(document.createTextNode(w.document.title + ' as of ' + now + ' in ' + navigator.userAgent)).parentElement);
+        w.document.body.appendChild(document.createElement('div').appendChild(consoleHistoryDownloadButton).parentElement);
+        ch.forEach(function(command, index) {
+            var pre = document.createElement('pre');
+//            pre.setAttribute('style', 'display: inline; border: 1px solid silver; margin: 1em;');
+           pre.setAttribute('style', 'margin: 0em;');
+            var div = document.createElement('div');
+            div.setAttribute('style', 'margin: 0.25em;');
+            pre.setAttribute('line-number', index + 1);
+            pre.contentEditable = true;
+            setupEventListeners(pre);
+            pre.innerText = command;
+            w.document.body.appendChild(div.appendChild(pre).parentElement);
+//             w.document.body.appendChild(pre);
+            console.log(command);
+        });
+        consoleHistoryDownloadButton.addEventListener('click', function(event) {
+            var consoleHistoryDocumentBlob = new window.Blob([
+                w.document.documentElement.innerHTML], {
+                'type': 'text/utf-8'
+            });
+            var a = document.createElement('a');
+            a.href = URL.createObjectURL(consoleHistoryDocumentBlob);
+            a.download = "consoleHistory_" + (now).toJSON().replace(/:/g, '') + '.html';
+            a.click();
+        }, false);
+    } catch (exception) {
+        alertAndWarn(exception.stack.replace(/:(\d+):(\d+)/g, "$& (Line $1, Column $2)"));
+    }
+}
         var title = 'DevTools Import/Export';
         var cancel = 'Cancel';
         var alertAndWarn = function(message) {
@@ -36,23 +144,32 @@
         div.appendChild(document.createElement('div').appendChild(localStorageDownloadButton).parentElement);
         var aYes = document.createElement('a');
         var aNo = document.createElement('a');
-        aYes.innerText = 'Open' + title;
+        aYes.innerText = 'Open ' + title;
         aNo.innerText = cancel;
         aYes.href = '';
         aNo.href = '';
+        aYes.target = 'dtie_dt';
+        aNo.target = '';
         var confirmerDiv = document.createElement('div');
         confirmerDiv.appendChild(document.createElement('div').appendChild(aYes).parentElement);
         confirmerDiv.appendChild(document.createElement('div').appendChild(aNo).parentElement);
         confirmerDiv.setAttribute('style', 'position:fixed; top: 25%; left: 25%; width: 50%; height: 50%; padding: 1em; border: 0.2em solid silver; background: white; font-size: large; z-index: 100;');
         if (location.origin === "chrome-devtools://devtools") {
-            var wa = window.open("", "", "width=300,height=50");
+            var wa = window.open("", "", "width=300,height=50,left=" + window.screen.availWidth / 2 + ",top=" + window.screen.availHeight / 2);
             wa.document.title = title;
+//             window.alert("Please confirm " + title + " to access data for " + location.origin);
             var confirmerDivPopup = wa.document.createElement('div');
             confirmerDivPopup.appendChild(document.createElement('div').appendChild(aYes).parentElement);
             confirmerDivPopup.appendChild(document.createElement('div').appendChild(aNo).parentElement);
             wa.document.body.appendChild(confirmerDivPopup);
         } else {
-            document.body.appendChild(confirmerDiv);
+//             if (document.hasFocus()) {
+                document.body.appendChild(confirmerDiv);
+//                 div.appendChild(document.createTextNode('To import/export devtools source snippets:\nUndock Developer tools into separate window\nand press Ctrl+Shift+I to inspect it.\nThen run this snippet from there.'));
+                window.alert('To import/export devtools source snippets:\nUndock Developer tools into separate window\nand press Ctrl+Shift+I to inspect it.\nThen run this snippet from there.');
+//             } else {
+//                 alertAndWarn("Select tab for " + location.href);
+//             }
         }
         var removeConfirmer = function(confirmerDiv) {
             document.body.removeChild(confirmerDiv);
@@ -75,13 +192,33 @@
             }
         }, false);
         var devtoolsImportExport = function() {
-            var w = window.open("", "dtie_dt");
+            var w = window.open("", "");
+//             try {
+//                 window.open("chrome://settings/search#download");
+//             } catch (exception) {
+//                 alertAndWarn(exception.stack);
+//             }
+//             var w = window.open("", "dtie_dt");
             //             var w = window.open("", "_blank", "width=400,height=500,top=100,left=100");
             w.document.title = title;
             w.document.body.appendChild(document.createElement('div').appendChild(div).parentElement);
             div.appendChild(document.createElement('hr'));
             //            settings.location.href = "chrome://extensions";
             if (location.origin === "chrome-devtools://devtools") {
+                if (localStorage.consoleHistory) {
+                    var aHistory = document.createElement('a');
+                    aHistory.href = '';
+                    aHistory.target = 'dtie_ch';
+                    aHistory.innerText = 'devtools console history';
+                    var aHistoryDiv = document.createElement('div');
+                    div.appendChild(aHistoryDiv);
+                    aHistoryDiv.appendChild(aHistory);
+                aHistoryDiv.addEventListener('click', function(event) {
+            event.preventDefault();
+                    downloadConsoleHistory();
+                }, false);
+                div.appendChild(document.createElement('hr'));
+                }
                 var importSnippets = document.createElement('input');
                 importSnippets.type = 'file';
                 importSnippets.multiple = true;
@@ -92,13 +229,13 @@
                 exportButton.value = 'Export All ' + snippets.length + ' Snippets';
 
                 //             div.appendChild(document.createElement('div').appendChild(document.createTextNode('Set Google Chrome to ask for download location to avaid malware warning')).parentElement);
-                var aa = document.createElement('div');
-                aa.innerHTML = 'Set Google Chrome to ask for download location (visit <a href="chrome:settings/search#download">chrome:settings/search#download</a>) to avoid malware warning';
-                div.appendChild(aa);
                 div.appendChild(document.createElement('div').appendChild(document.createTextNode('Import Snippets')).parentElement);
                 div.appendChild(importSnippets);
                 div.appendChild(document.createElement('hr'));
                 div.appendChild(document.createElement('div').appendChild(exportButton).parentElement);
+                var aa = document.createElement('div');
+                aa.innerHTML = 'Set Google Chrome to ask for download location (visit <a href="chrome://settings/search#download">chrome://settings/search#download</a>) to avoid malware warning';
+                div.appendChild(aa);
                 div.appendChild(document.createElement('div').appendChild(document.createTextNode('Export Snippets Individually')).parentElement);
                 snippets.forEach(function(snippet) {
                     var blob = new window.Blob(['// snippet ' + snippet.name + ' exported by snippeteer from\n// ' + navigator.userAgent + '\n// at ' + (new Date()).toJSON() + '\n' + snippet.content], {
@@ -219,9 +356,10 @@
                         w.document.body.classList.remove('valid');
                     }
                 }, false && "useCapture"); //$NON-NLS-0$ //$NON-NLS-1$
-            } else {
-                div.appendChild(document.createTextNode('Undock Developer tools into separate window and press Ctrl+Shift+I to inspect it. Then run snipetter from there to import/export snippets.'));
             }
+//             else {
+//                 div.appendChild(document.createTextNode('Undock Developer tools into separate window and press Ctrl+Shift+I to inspect it. Then run snipetter from there to import/export snippets.'));
+//             }
         }
         //         aYes.click();
     } catch (exception) {
