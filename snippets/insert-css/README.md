35b8aaa68656f79920900e270983e29966a6bfe9
Alex Dergachev <alex@evolvingweb.ca>
2013-11-18 13:12:27 -0500

    Add snippets/insert-css

---
+++ snippets/insert-css/README.md
@@ -0,0 +1,10 @@
+### insert-css.js
+
+Defines `insertCss()` function which injects a snippet of CSS into the current
+page.  After running the snippet, you can call it as follows:
+
+```js
+insertCss('span { color: red !important; }');
+```
+
+[![insert-css](insert-css.png)](insert-css.js)
---
+++ snippets/insert-css/insert-css.js
@@ -0,0 +1,23 @@
+function insertCss(code) {
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (style.styleSheet) {  // IE
+    style.styleSheet.cssText = code;
+  } else { // Other browsers
+    style.innerHTML = code;
+  }
+  document.getElementsByTagName("head")[0].appendChild( style );
+}
+
+// Feel free to extend this snippet with your favorite CSS snippets.
+// Here's an example; which makes the current page high contrast.
+// Notice the trailing backslashes, used to define multiline strings.
+insertCssHighContrast = function() {
+  var css = '\
+    * { background: white ! important; color: black !important } \
+    :link, :link * { color: #0000EE !important } \
+    :visited, :visited * { color: #551A8B !important } \
+  ';
+    insertCss(css);
+}
