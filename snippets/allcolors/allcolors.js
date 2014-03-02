8e98a97dd2b3a7053331356e139fff5ef925ff0f
Brian Grinstead <briangrinstead@gmail.com>
2014-03-02 10:35:33 -0600

    reorganize allcolors

--- snippets/allcolors/allcolors.js
+++ snippets/allcolors/allcolors.js
@@ -2,10 +2,10 @@
 // https://github.com/bgrins/devtools-snippets
 // Print out CSS colors used in elements on the page.
 
-//Parameter used to include or exclude colors from elements that have a border color but have a zero width.
-var includeBorderColorsWithZeroWidth = false;
+(function () {
+    // Should include colors from elements that have a border color but have a zero width?
+    var includeBorderColorsWithZeroWidth = false;
 
-(function (includeBorderColorsWithZeroWidth) {
     var allColors = {};
     var props = ["background-color", "color", "border-top-color", "border-right-color", "border-bottom-color", "border-left-color"];
     var skipColors = {
@@ -17,8 +17,7 @@ var includeBorderColorsWithZeroWidth = false;
     [].forEach.call(document.querySelectorAll("*"), function (node) {
         var nodeColors = {};
         props.forEach(function (prop) {
-            var
-            color = window.getComputedStyle(node, null).getPropertyValue(prop),
+            var color = window.getComputedStyle(node, null).getPropertyValue(prop),
                 thisIsABorderProperty = (prop.indexOf("border") != -1),
                 notBorderZero = thisIsABorderProperty ? window.getComputedStyle(node, null).getPropertyValue(prop.replace("color", "width")) !== "0px" : true,
                 colorConditionsMet;
@@ -35,50 +34,40 @@ var includeBorderColorsWithZeroWidth = false;
                         count: 0,
                         nodes: []
                     };
-                } // if (!allColors[color])
+                }
 
                 if (!nodeColors[color]) {
                     allColors[color].count++;
                     allColors[color].nodes.push(node);
-                } // if (!nodeColors[color])
+                }
 
                 nodeColors[color] = true;
-                
-            } // if if (colorConditionsMet)
-
-        }); // props.forEach(function (prop)
-
-    }); // [].forEach.call(document.querySelectorAll("*"), function (node)
-
-
-
-    var rgbTextToRgbArray = (function (rgbText) {
-        return function (rgbText) {
-            var rgbString;
-
-            if (rgbText.indexOf("rgba") == -1) {
-                rgbString = rgbText.replace("rgb(", "").replace(")", "");
-            } else {
-                rgbString = rgbText.replace("rgba(", "").replace(")", "");
             }
+        });
+    });
 
-            var arrayOfRgbValues = rgbString.split(",").map(function (item) {
-                return parseInt(item, 10);
-            });
+    function rgbTextToRgbArray(rgbText) {
+        var rgbString;
 
-            return arrayOfRgbValues;
+        if (rgbText.indexOf("rgba") == -1) {
+            rgbString = rgbText.replace("rgb(", "").replace(")", "");
+        } else {
+            rgbString = rgbText.replace("rgba(", "").replace(")", "");
+        }
 
-        } // return function(rgbText)
+        var arrayOfRgbValues = rgbString.split(",").map(function (item) {
+            return parseInt(item, 10);
+        });
 
-    })(); // var rgbTextToRgbArray = (function (rgbText)
+        return arrayOfRgbValues;
+    }
 
-
-    var componentToHex = function (c) {
+    function componentToHex(c) {
         var hex = c.toString(16);
         return hex.length == 1 ? "0" + hex : hex;
     }
 
-    var rgbToHex = function (rgbArray) {
+    function rgbToHex(rgbArray) {
         var r = rgbArray[0],
             g = rgbArray[1],
             b = rgbArray[2];
@@ -92,28 +81,22 @@ var includeBorderColorsWithZeroWidth = false;
         var rgbArray = rgbTextToRgbArray(i),
             hexValue = rgbToHex(rgbArray);
 
-
         allColorsSorted.push({
             key: i,
             value: allColors[i],
             hexValue: hexValue
         });
-
-    } // for (var i in allColors)
+    }
 
     allColorsSorted = allColorsSorted.sort(function (a, b) {
         return b.value.count - a.value.count;
     });
-
-
 
     var nameStyle = "font-weight:normal;";
     var countStyle = "font-weight:bold;";
     var colorStyle = function (color) {
         return "background:" + color + ";color:" + color + ";border:1px solid #333;";
     };
-
-
 
     console.group("All colors used in elements on the page");
     allColorsSorted.forEach(function (c) {
@@ -125,4 +108,5 @@ var includeBorderColorsWithZeroWidth = false;
         console.groupEnd();
     });
     console.groupEnd("All colors used in elements on the page");
-})(includeBorderColorsWithZeroWidth);
+
+})();
