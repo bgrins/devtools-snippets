0c081f5710228dc30b2a0bdc1a031e0ea7c88685
Henrique Valcanaia <henrique.valcanaia@hotmail.com>
2014-09-06 13:07:29 -0300

    Added function to window object
    Function viewCookies added to window object.

--- snippets/viewcookies/viewcookies.js
+++ snippets/viewcookies/viewcookies.js
@@ -2,17 +2,17 @@
 // https://github.com/bgrins/devtools-snippets
 // Shows all cookies stored in document.cookies in a console.table
 
-(function(){
+(function() {
+    window.viewCookies = function() {
+        var rawCookies = document.cookie.split(';'), cookies = [];
 
-  var rawCookies = document.cookie.split(';'),
-      cookies = [];
-
-  rawCookies.forEach(function(cookie) {
-    var parsedCookie = cookie.split('='),
-        cookieData = {};
-    cookies.push({ 'key': parsedCookie.shift(), 'value': decodeURIComponent(parsedCookie.join('=')) });
-  });
-
-  console.table(cookies);
-
-})()
+        rawCookies.forEach(function(cookie) {
+            var parsedCookie = cookie.split('='), cookieData = {};
+            cookies.push({
+                'key': parsedCookie.shift(),
+                'value': decodeURIComponent(parsedCookie.join('='))
+            });
+        });
+        console.table(cookies);
+    };
+})();
