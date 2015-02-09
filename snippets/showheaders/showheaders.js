b8c88219f48f4fac000e8217eb9b5d5ced356525
awalGarg <aakarsh1997@gmail.com>
2015-02-10 03:00:37 +0530

    made request asynchronous
    removes that ugly warning in the console about the detrimental effects of synchronous xhrs

--- snippets/showheaders/showheaders.js
+++ snippets/showheaders/showheaders.js
@@ -5,17 +5,20 @@
 (function() {
 
   var request=new XMLHttpRequest();
-  request.open('HEAD',window.location,false);
+  request.open('HEAD',window.location,true);
+  
+  request.onload = request.onerror = function () {
+    var headers = request.getAllResponseHeaders();
+    var tab = headers.split("\n").map(function(h) {
+      return { "Key": h.split(": ")[0], "Value": h.split(": ")[1] }
+    }).filter(function(h) { return h.Value !== undefined; });
+
+    console.group("Request Headers");
+    console.log(headers);
+    console.table(tab);
+    console.groupEnd("Request Headers");
+  };
+  
   request.send(null);
-
-  var headers = request.getAllResponseHeaders();
-  var tab = headers.split("\n").map(function(h) {
-    return { "Key": h.split(": ")[0], "Value": h.split(": ")[1] }
-  }).filter(function(h) { return h.Value !== undefined; });
-
-  console.group("Request Headers");
-  console.log(headers);
-  console.table(tab);
-  console.groupEnd("Request Headers");
 
 })();
